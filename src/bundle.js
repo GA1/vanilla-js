b8fc18aa15efc8cb445267ba0822703b30d7b4cc
GA1 <nvkga1@gmail.com>
2018-04-15 23:55:06 +0200

    Update price when checking/unchecking rooms

--- src/bundle.js
+++ src/bundle.js
@@ -1,12 +1,50 @@
-[
+var rooms = [
   {
     "type": "luxury",
+    "amenities": ["fridge", "tv", "internet"],
     "price": 20,
-    "amenities": ["fridge", "tv, internet"]
   },
   {
     "type": "regular",
+    "amenities": ["fridge", "internet"],
     "price": 30,
-    "amenities": ["fridge", "internet"]
   }
 ]
+
+var sum = 0
+
+function addSubtractPrice(e) {
+  var target = e.target;
+  var price = target.value;
+  if (target.checked)
+    sum += Number(price)
+  else
+    sum -= Number(price)
+  updatePrice()
+}
+
+function updatePrice() {
+  $("#price").html(sum);
+}
+
+$(document).ready(function(){
+  $("#welcome-message").html("Choose room(s):");
+
+  function createHtmlRoomRow(r) {
+    var price = r['price'];
+    var amenities = r['amenities'].join(' ');
+    var type = r['type'];
+    var result =
+      "<tr> " +
+          "<td>" + type + "</td> " +
+          "<td>" + amenities + "</td>" +
+          "<td>" + price + "</td>" +
+          '<td><input value="' + price + '"type="checkbox" onclick="addSubtractPrice(event)"></td>' +
+      "</tr>";
+    return result
+  };
+
+  for (var i = 0; i < rooms.length; i++) {
+    $("#room-table").append(createHtmlRoomRow(rooms[i]));
+  }
+});

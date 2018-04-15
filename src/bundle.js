acabcd9319a644e8b1431cf8b9aba58b2174b4d9
GA1 <nvkga1@gmail.com>
2018-04-15 21:50:16 +0200

    Show static table

---
+++ src/bundle.js
@@ -0,0 +1,12 @@
+[
+  {
+    "type": "luxury",
+    "price": 20,
+    "amenities": ["fridge", "tv, internet"]
+  },
+  {
+    "type": "regular",
+    "price": 30,
+    "amenities": ["fridge", "internet"]
+  }
+]
